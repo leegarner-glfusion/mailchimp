05bc0b8d2162a75ee8cedce92d33d78c8eb4524f
Lee Garner <lee@leegarner.com>
2019-01-01 07:47:11 -0800

    Update installation, upgrade and add dvlpupdate

--- install_defaults.php
+++ install_defaults.php
@@ -8,9 +8,9 @@
  * upgrading
  *
  * @author     Lee Garner <lee@leegarner.com>
- * @copyright  Copyright (c) 2012 Lee Garner <lee@leegarner.com>
+ * @copyright  Copyright (c) 2012-2018 Lee Garner <lee@leegarner.com>
  * @package    mailchimp
- * @version    0.0.1
+ * @version    0.1.0
  * @license    http://opensource.org/licenses/gpl-2.0.php
  *             GNU Public License v2 or later
  * @filesource
@@ -20,19 +20,172 @@ if (!defined ('GVERSION')) {
     die('This file can not be used on its own!');
 }
 
-global $_MLCH_DEFAULT;
-$_MLCH_DEFAULT = array(
-    'api_key'   =>  '',     // Mailchimp API key
-    'def_list'  =>  '',     // Default list ID
-    'sub_register' => 0,    // Offer subscription during registration
-    'del_user_unsub' => 0,  // deleted users are unsubscribed
-    'dbl_optin_members' => 1,    // Require doubld-opt-in for profile changes
-    'sync_at_login' => 1,   // Sync with MC at user login?
-    'webhook_key' => '',    // Key value for webhook. Must be "?key=" in url
-    'webhook_handlesub' => 1,   // handle Mailchimp subscriptions
-    'webhook_handleunsub' => 1, // handle Mailchimp unsubscribe
-    'webhook_handleupemail' => 0,  // Update email address in 'users' table
-    'debug' => 0,           // 1 to enable webhook debugging
+/**
+ * Mailchimp default settings
+ *
+ * Initial Installation Defaults used when loading the online configuration
+ * records. These settings are only used during the initial installation
+ * and not referenced any more once the plugin is installed
+ *
+ * @global  array
+ */
+$mailchimpConfigData = array(
+    array(
+        'name' => 'sg_main',
+        'default_value' => NULL,
+        'type' => 'subgroup',
+        'subgroup' => 0,
+        'fieldset' => 0,
+        'selection_array' => NULL,
+        'sort' => 0,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+    array(
+        'name' => 'fs_main',
+        'default_value' => NULL,
+        'type' => 'fieldset',
+        'subgroup' => 0,
+        'fieldset' => 0,
+        'selection_array' => NULL,
+        'sort' => 0,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+    array(
+        'name' => 'api_key',
+        'default_value' => '',
+        'type' => 'text',
+        'subgroup' => 0,
+        'fieldset' => 0,
+        'selection_array' => 0,
+        'sort' => 10,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+    array(
+        'name' => 'def_list',
+        'default_value' => '',
+        'type' => 'text',
+        'subgroup' => 0,
+        'fieldset' => 0,
+        'selection_array' => 0,
+        'sort' => 20,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+    array(
+        'name' => 'sub_register',
+        'default_value' => 0,
+        'type' => 'select',
+        'subgroup' => 0,
+        'fieldset' => 0,
+        'selection_array' => 4,
+        'sort' => 30,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+    array(
+        'name' => 'del_user_unsub',
+        'default_value' => 0,
+        'type' => 'select',
+        'subgroup' => 0,
+        'fieldset' => 0,
+        'selection_array' => 0,
+        'sort' => 40,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+    array(
+        'name' => 'dbl_optin_members',
+        'default_value' => 1,
+        'type' => 'select',
+        'subgroup' => 0,
+        'fieldset' => 0,
+        'selection_array' => 0,
+        'sort' => 50,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+    array(
+        'name' => 'sync_at_login',
+        'default_value' => 1,
+        'type' => 'select',
+        'subgroup' => 0,
+        'fieldset' => 0,
+        'selection_array' => 0,
+        'sort' => 60,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+
+
+    array(
+        'name' => 'fs_webhook',
+        'default_value' => NULL,
+        'type' => 'fieldset',
+        'subgroup' => 0,
+        'fieldset' => 10,
+        'selection_array' => NULL,
+        'sort' => 10,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+    array(
+        'name' => 'webhook_key',
+        'default_value' => '',
+        'type' => 'text',
+        'subgroup' => 0,
+        'fieldset' => 10,
+        'selection_array' => 0,
+        'sort' => 10,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+    array(
+        'name' => 'webhook_handlesub',
+        'default_value' => 1,
+        'type' => 'select',
+        'subgroup' => 0,
+        'fieldset' => 10,
+        'selection_array' => 0,
+        'sort' => 20,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+    array(
+        'name' => 'webhook_handleunsub',
+        'default_value' => 1,
+        'type' => 'select',
+        'subgroup' => 0,
+        'fieldset' => 10,
+        'selection_array' => 0,
+        'sort' => 30,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+    array(
+        'name' => 'webhook_handleupemail',
+        'default_value' => 0,
+        'type' => 'select',
+        'subgroup' => 0,
+        'fieldset' => 10,
+        'selection_array' => 0,
+        'sort' => 40,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
+    array(
+        'name' => 'debug',
+        'default_value' => 0,
+        'type' => 'select',
+        'subgroup' => 0,
+        'fieldset' => 10,
+        'selection_array' => 0,
+        'sort' => 50,
+        'set' => true,
+        'group' => 'mailchimp',
+    ),
 );
 
 
@@ -47,52 +200,16 @@ $_MLCH_DEFAULT = array(
  */
 function plugin_initconfig_mailchimp()
 {
-    global $_CONF_MLCH, $_MLCH_DEFAULT;
-
-    if (is_array($_CONF_MLCH) && (count($_CONF_MLCH) > 1)) {
-        $_MLCH_DEFAULT = array_merge($_MLCH_DEFAULT, $_CONF_MLCH);
-    }
+    global $mailchimpConfigData;
 
     $c = config::get_instance();
-    if (!$c->group_exists($_CONF_MLCH['pi_name'])) {
-
-        $c->add('sg_main', NULL, 'subgroup',
-                0, 0, NULL, 0, true, $_CONF_MLCH['pi_name']);
-        $c->add('fs_main', NULL, 'fieldset',
-                0, 0, NULL, 0, true, $_CONF_MLCH['pi_name']);
-
-        $c->add('api_key', $_MLCH_DEFAULT['api_key'], 'text',
-                0, 0, 0, 10, true, $_CONF_MLCH['pi_name']);
-        $c->add('def_list', $_MLCH_DEFAULT['def_list'], 'select',
-                0, 0, 0, 20, true, $_CONF_MLCH['pi_name']);
-        $c->add('sub_register', $_MLCH_DEFAULT['sub_register'], 'select',
-                0, 0, 4, 30, true, $_CONF_MLCH['pi_name']);
-        $c->add('del_user_unsub', $_MLCH_DEFAULT['del_user_unsub'], 'select',
-                0, 0, 0, 40, true, $_CONF_MLCH['pi_name']);
-        $c->add('dbl_optin_members', $_MLCH_DEFAULT['dbl_optin_members'], 'select',
-                0, 0, 0, 50, true, $_CONF_MLCH['pi_name']);
-        $c->add('sync_at_login', $_MLCH_DEFAULT['sync_at_login'], 'select',
-                0, 0, 0, 50, true, $_CONF_MLCH['pi_name']);
-
-        // Mailchimp webhook integrations
-        $c->add('fs_webhook', NULL, 'fieldset',
-                0, 10, NULL, 0, true, $_CONF_MLCH['pi_name']);
-
-        $c->add('webhook_key', $_MLCH_DEFAULT['webhook_key'],
-                'text', 0, 10, 0, 100, true, $_CONF_MLCH['pi_name']);
-        $c->add('webhook_handlesub', $_MLCH_DEFAULT['webhook_handlesub'],
-                'select', 0, 10, 0, 110, true, $_CONF_MLCH['pi_name']);
-        $c->add('webhook_handleunsub', $_MLCH_DEFAULT['webhook_handleunsub'],
-                'select', 0, 10, 0, 120, true, $_CONF_MLCH['pi_name']);
-        $c->add('webhook_handleupemail', $_MLCH_DEFAULT['webhook_handleupemail'],
-                'select', 0, 10, 0, 130, true, $_CONF_MLCH['pi_name']);
-        $c->add('debug', $_MLCH_DEFAULT['debug'],
-                'select', 0, 10, 0, 140, true, $_CONF_MLCH['pi_name']);
-
-        return true;
-    } else {
-        return false;
+    if (!$c->group_exists('mailchimp')) {
+        USES_lib_install();
+        foreach ($mailchimpConfigData AS $cfgItem) {
+            _addConfigItem($cfgItem);
+        }
     }
+    return true;
 }
 
 ?>
--- upgrade.inc.php
+++ upgrade.inc.php
@@ -14,26 +14,43 @@
 // Required to get the config values
 global $_CONF, $_MLCH_CONF, $_DB_dbms;
 
-/** Include database definitions */
-require_once dirname(__FILE__) . '/sql/'. $_DB_dbms. '_install.php';
-
-
 /**
  * Perform the upgrade starting at the current version.
- * This plugin has no tables but may get new configuration items.
  *
- * @param   string  $current_ver    Current installed version to be upgraded
+ * @param   boolean $dvlp   True for development update, ignore errors
  * @return  integer                 Error code, 0 for success
  */
-function MLCH_do_upgrade($current_ver)
+function MLCH_do_upgrade($dvlp=false)
 {
     global $_MLCH_CONF;
 
-    $error = 0;
+    $pi_name = $_MLCH_CONF['pi_name'];
+    if (isset($_PLUGIN_INFO[$pi_name])) {
+        if (is_array($_PLUGIN_INFO[$pi_name])) {
+            // glFusion >= 1.6.6
+            $current_ver = $_PLUGIN_INFO[$pi_name]['pi_version'];
+        } else {
+            // legacy
+            $current_ver = $_PLUGIN_INFO[$pi_name];
+        }
+    } else {
+        return false;
+    }
+    $installed_ver = plugin_chkVersion_mailchimp();
 
-    if ($current_ver < '0.0.3') {
-        $error = MLCH_upgrade_0_0_3();
-        if ($error) return $error;
+    if (!COM_checkVersion($current_ver, '0.0.3')) {
+        // upgrade to 0.0.3
+        $current_ver = '0.0.3';
+        if (!MLCH_upgrade_0_0_3()) return false;
+    }
+
+    // Update the plugin configuration
+    USES_lib_install();
+    global $mailchimpConfigData;
+    require_once __DIR__ . '/install_defaults.php';
+    _update_config('mailchimp', $mailchimpConfigData);
+    if (!COM_checkVersion($current_ver, $installed_ver)) {
+        if (!MLCH_do_set_version($installed_ver)) return false;
     }
 
     return $error;
@@ -71,7 +88,7 @@ function MLCH_upgrade_0_0_3()
     }
 
     $status = MLCH_do_upgrade_sql('0.0.3');
-    if ($status == 0) {
+    if ($status) {
         // If upgrade went ok, sync our cache to the current Mailchimp list
         MLCH_sync_cache();
     }
@@ -84,14 +101,14 @@ function MLCH_upgrade_0_0_3()
  * If there are no SQL statements, then SUCCESS is returned.
  *
  * @param   string  $version    Version being upgraded TO
- * @param   array   $sql        Array of SQL statement(s) to execute
+ * @param   boolean $dvlp       True to ignore errors
  * @return  boolean     True on success, False on failure
  */
-function MLCH_do_upgrade_sql($version = 'Undefined', $sql='')
+function MLCH_do_upgrade_sql($version = 'Undefined', $dvlp=false
 {
     global $_TABLES, $_CONF_MLCH, $_MLCH_UPGRADE_SQL;
 
-    require_once dirname(__FILE__) . '/install_defaults.php';
+    require_once __DIR__  . '/sql/mysql_install.php';
 
     // Execute a single SQL query, if supplied.
     if (!empty($sql)) {
@@ -102,7 +119,7 @@ function MLCH_do_upgrade_sql($version = 'Undefined', $sql='')
     // We control this, so it shouldn't happen, but just to be safe...
     if ($version == 'Undefined') {
         COM_errorLog("Error updating {$_CONF_MLCH['pi_name']} - Undefined Version");
-        return 1;
+        return false;
     }
 
     // If no sql statements passed in, return success
@@ -116,11 +133,42 @@ function MLCH_do_upgrade_sql($version = 'Undefined', $sql='')
         DB_query($sql, '1');
         if (DB_error()) {
             COM_errorLog("SQL Error during Mailchimp plugin update",1);
-            return false;
-            break;
+            if (!$dvlp) return false;
         }
     }
     return true;
 }
+
+
+/**
+ * Update the plugin version number in the database.
+ * Called at each version upgrade to keep up to date with
+ * successful upgrades.
+ *
+ * @param   string  $ver    New version to set
+ * @return  boolean         True on success, False on failure
+ */
+function MLCH_do_set_version($ver)
+{
+    global $_TABLES, $_MLCH_CONF;
+
+    $ver = DB_escapeString($ver);
+    // now update the current version number.
+    $sql = "UPDATE {$_TABLES['plugins']} SET
+            pi_version = '$ver',
+            pi_gl_version = '{$_MLCH_CONF['gl_version']}',
+            pi_homepage = '{$_MLCH_CONF['pi_url']}'
+        WHERE pi_name = '{$_MLCH_CONF['pi_name']}'";
+
+    $res = DB_query($sql, 1);
+    if (DB_error()) {
+        COM_errorLog("Error updating the {$_MLCH_CONF['pi_display_name']} Plugin version to $ver",1);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
 
 ?>
