50a4d1ab23aaea85c05be6719e4388ffbaf2919b
Lee Garner <lee@leegarner.com>
2021-01-11 09:58:51 -0800

    Shorten listid filed in installation

--- sql/mysql_install.php
+++ sql/mysql_install.php
@@ -4,7 +4,7 @@
  * @author      Lee Garner <lee@leegarner.com>
  * @copyright   Copyright (c) 2013-2020 Lee Garner <lee@leegarner.com>
  * @package     mailchimp
- * @version     v0.1.0
+ * @version     v0.2.0
  * @license     http://opensource.org/licenses/gpl-2.0.php
  *              GNU Public License v2 or later
  * @filesource
@@ -12,7 +12,7 @@
 
 $_SQL['mailchimp_cache'] = "CREATE TABLE {$_TABLES['mailchimp_cache']} (
   `uid` int(11) NOT NULL,
-  `listid` varchar(255) NOT NULL,
+  `listid` varchar(20) NOT NULL,
   `subscribed` tinyint(1) unsigned default 1,
   PRIMARY KEY (`uid`, `listid`)
 )";
